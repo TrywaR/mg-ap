65e8d9060d9be4992404686be895d6eff20880cb
TrywaR <TrywaR@yandex.ru>
2020-01-16 02:16:49 +0300

    login detected

--- www/js/authorizations/authorization.js
+++ www/js/authorizations/authorization.js
@@ -9,7 +9,9 @@ function authorization(){
     // console.log('start')
     active_buttons()
     content_upload('templates/authorizations/authorization.htm')
+    // content_upload('templates/authorizations/registration.htm')
     // content_upload('https://alliance.paultik.ru/account/registration/')
+    // content_upload('https://alliance.paultik.ru/api/v1/accounts/login/')
   }
 }
 // authorization x
@@ -17,40 +19,41 @@ function authorization(){
 // authorization_message
 function authorization_message(type,txt){
   type = type ? type : 0
-  $(document).find('.authorization_forms .form_status').removeClass('_error')
-  if ( type == 0 ) $(document).find('.authorization_forms .form_status').html('')
+  $(document).find('.authorization_form .form_status').removeClass('_error')
+  if ( type == 0 ) $(document).find('.authorization_form .form_status').html('')
 
   // message
-  if ( type == 0 ) $(document).find('.authorization_forms .form_status').html(txt)
+  if ( type == 0 ) $(document).find('.authorization_form .form_status').html(txt)
   // error
-  if ( type == 1 ) $(document).find('.authorization_forms .form_status').addClass('_error').html(txt)
+  if ( type == 1 ) $(document).find('.authorization_form .form_status').addClass('_error').html(txt)
 }
 // authorization_message x
 
 $(function(){
   authorization()
+  console.log('detected')
 
   // authorization_form
   $(document).on('submit', '#authorization_form', function(){
     // - Получаем ключ сессии
     // - Параметры
     var
-    // data = [], // Отправляемые данные
+    data = [], // Отправляемые данные
     result_text = '', // Текст о результате
     validate = 0 // Если есть ошибки
 
     // - demo
-    login = true
-    if ( $(this).find('[name=email]').val() != 'demo' ) login = false
-    if ( $(this).find('[name=password]').val() != 'demo' ) login = false
-    if ( login ) {
-      localStorage.setItem('session_key', session_key = 'demo')
-      authorization()
-    }else{
-      validate = 1
-      result_text = 'Email или пароль введён не верно!'
-      authorization_message(validate, result_text)
-    }
+    // login = true
+    // if ( $(this).find('[name=email]').val() != 'demo' ) login = false
+    // if ( $(this).find('[name=password]').val() != 'demo' ) login = false
+    // if ( login ) {
+    //   localStorage.setItem('session_key', session_key = 'demo')
+    //   authorization()
+    // }else{
+    //   validate = 1
+    //   result_text = 'Email или пароль введён не верно!'
+    //   authorization_message(validate, result_text)
+    // }
     // - demo x
 
     // - site_login
@@ -62,36 +65,46 @@ $(function(){
     // })
     // data = $.param(data)
     //
-    // $.ajax({
-    //   url: site_url + 'account/login/',
-    //   data: data,
-    //   method: 'POST',
-    //   // headers: {
-    //   //   'X-CSRF-TOKEN': csrftoken
-    //   // }
-    // }).fail(function(data) {
-    //   authorization_message(1, '<p style="color:#ff7f7f;">' + data.status + '<br/>' + data.statusText + '</p>')
-    //
-    // }).done(function(data) {
-    //   authorization_message(1, '<p style="color:#ff7f7f;">' + data.status + '<br/>' + data.statusText + '</p>')
-    //
-    // })
+
+    data = $(this).serializeArray()
+
+    $.ajax({
+      url: site_url + 'api/v1/accounts/login/',
+      data: data,
+      method: 'POST',
+      // headers: {
+      //   'X-CSRF-TOKEN': csrftoken
+      // }
+    }).fail(function(data) {
+      // - Парсим ошибки
+      if ( data.responseJSON ) {
+        // -- Ошибка, логин или пароль не подходит
+        if ( data.responseJSON.detail ) {
+          console.log( data.responseJSON.detail )
+          authorization_message(1, '<p style="color:#ff7f7f;">' + data.responseJSON.detail + '</p>')
+          return false
+        }
+      }
+      console.log(data)
+      return false
+
+    }).done(function(data) {
+      // - Успешно вошли, 300 чашек чаю, этому господину
+      console.log(data)
+
+      if ( data.token ) {
+        localStorage.setItem('session_key', session_key = data.token)
+        authorization()
+
+        if ( data.detail )
+        authorization_message(1, '<p style="color:#ff7f7f;">' + data.detail + '</p>')
+      }
+    })
     // - site_login x
 
     return false
   })
   // authorization_form х
-
-  // registration_form
-  $(document).on('submit', '#registration_form', function(data){
-    // if ( $(this).find('[name=code]').val() ) {
-    //   if ($(this).find('[name=code]').val() == 'demo' ) {
-    //     content_upload('templates/authorizations/')
-    //   }
-    // }
-    return false
-  })
-  // registration_form x
 
   // password_form
   $(document).on('submit', '#password_form', function(){
---
+++ www/js/authorizations/registration.js
@@ -0,0 +1,114 @@
+// registration_message
+function registration_message(type,txt){
+  type = type ? type : 0
+  $(document).find('.registration_form .form_status').removeClass('_error')
+  if ( type == 0 ) $(document).find('.registration_form .form_status').html('')
+
+  // message
+  if ( type == 0 ) $(document).find('.registration_form .form_status').html(txt)
+  // error
+  if ( type == 1 ) $(document).find('.registration_form .form_status').addClass('_error').html(txt)
+}
+// registration_message x
+
+$(function(){
+  // registration_form
+  $(document).on('submit', '#registration_form', function(){
+    // - Получаем ключ сессии
+    // - Параметры
+    var
+    data = [], // Отправляемые данные
+    result_text = '', // Текст о результате
+    validate = 0 // Если есть ошибки
+
+    // - demo
+    // login = true
+    // if ( $(this).find('[name=email]').val() != 'demo' ) login = false
+    // if ( $(this).find('[name=password]').val() != 'demo' ) login = false
+    // if ( login ) {
+    //   localStorage.setItem('session_key', session_key = 'demo')
+    //   registration()
+    // }else{
+    //   validate = 1
+    //   result_text = 'Email или пароль введён не верно!'
+    //   registration_message(validate, result_text)
+    // }
+    // - demo x
+
+    // - site_login
+    // -- add csrftoken
+    // data = $(this).serializeArray()
+    // data.push({
+    //   name: 'csrftoken',
+    //   value: csrftoken
+    // })
+    // data = $.param(data)
+
+    data = $(this).serializeArray()
+
+    $.ajax({
+      url: site_url + 'api/v1/accounts/register/',
+      data: data,
+      method: 'POST',
+      // headers: {
+      //   'X-CSRF-TOKEN': csrftoken
+      // }
+    }).fail(function(data) {
+      // - Парсим ошибки
+      if ( data.responseJSON ) {
+        // -- Email уже используется
+        if ( data.responseJSON.email ) {
+          var
+          error_messages = '<p style="color:#ff7f7f;">'
+
+          $.each(data.responseJSON.email, function(index, value){
+            error_messages += value + '<br/>'
+          })
+
+          error_messages += '</p>'
+          registration_message(1, error_messages)
+
+          return false
+        }
+        // -- Если пароли не совпадают
+        if ( data.responseJSON.non_field_errors ) {
+          var
+          error_messages = '<p style="color:#ff7f7f;">'
+
+          $.each(data.responseJSON.non_field_errors, function(index, value){
+            error_messages += value + '<br/>'
+          })
+
+          error_messages += '</p>'
+          registration_message(1, error_messages)
+
+          return false
+        }
+        // -- Если пароль не верен, выводим месседж
+        if ( data.responseJSON.password ) {
+          var
+          error_messages = '<p style="color:#ff7f7f;">'
+
+          $.each(data.responseJSON.password, function(index, value){
+            error_messages += value + '<br/>'
+          })
+
+          error_messages += '</p>'
+          registration_message(1, error_messages)
+
+          return false
+        }
+      }
+
+      // -- Другие ошибки
+      console.log(data)
+      registration_message(1, '<p style="color:#ff7f7f;">' + data + '</p>')
+
+    }).done(function(data) {
+      registration_message(1, '<p style="color:#ff7f7f;">' + '<br/>' + data.responseJSON.detail + '</p>')
+    })
+    // - site_login x
+
+    return false
+  })
+})
